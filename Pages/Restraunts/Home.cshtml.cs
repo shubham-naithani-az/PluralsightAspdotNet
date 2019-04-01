c8ddc6ef9046380d36a8ddd825cbe5dfd0005d1b
Shubham Naithani <shubham.naithani.az@hotmail.com>
2019-04-01 21:03:06 +0530

    First deployment

---
+++ Pages/Restraunts/Home.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace PluralSightASP.NET.Pages.Restraunts
+{
+    public class HomeModel : PageModel
+    {
+        private readonly IConfiguration Config;
+
+        public HomeModel(IConfiguration configuration)
+        {
+            this.Config = configuration;
+        }
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = Config["Message"];
+        }
+    }
+}
